aa220cd114cb209ac066a8ec571bc3571435f448
Megan Burton <meggie.burton@gmail.comn>
2014-04-21 21:38:01 -0600

    Writing makeCacheMatrix Function

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Put comments here that give an overall description of what your functions do
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse <- NULL
+    
+    set <- function(y = matrix()) {
+        x <<- y
+        inverse <<- NULL
+    }
+    
+    get <- function() {
+        x
+    }
+    
+    solveInverse <- function() {
+        inverse <<- solve(x)
+        inverse
+    }
+    
+    getInverse <- function() {
+        inverse
+    }
+    
+    list(set = set,
+         get =  get,
+         solveInverse = solveInverse,
+         getInverse = getInverse)    
 }
 
 
