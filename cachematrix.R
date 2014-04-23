ab92430d6e295f61abff428b81590eac0f871d9a
Megan Burton <meggie.burton@gmail.comn>
2014-04-22 21:48:20 -0600

    Revised comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,20 +1,20 @@
 ## The two functions that I have written work together to solve for and 
-## cache the inverse of a matrix.The first function, 'makeCacheMatrix',
+## cache the inverse of a matrix. The first function, 'makeCacheMatrix',
 ## takes a matrix and returns a special matrix object that provides the
 ## information needed to solve for and cache the inverse. The second function, 
 ## 'cacheSolve', returns the inverse of the matrix either by solving the inverse, 
 ## or by retrieving it from the cache if it has already been calculated.  
 
 ## 'makeCacheMatrix' is a function that takes an argument x - which is a matrix. 
-## If no argument is given, the defualt is an empty matrix. The overarching goal 
-## of this function is to calculate and store the inverse of x, which is does 
+## If no argument is given, the defualt is an empty matrix. The goal 
+## of this function is to calculate and store the inverse of x, which it does 
 ## by executing the 'solveInverse' function. 'makeCacheMatrix' includes two 'get' 
 ## functions that each return matrices. This function also includes a 'set' 
 ## function that allows a user to re-set the value of x. 
 
 makeCacheMatrix <- function(x = matrix()) {
     inverse <- NULL
-    ## creating a variable, inverse, and initializing its value to be NULL
+    ## creating a variable, inverse, and initializing its value to be NULL.
     
     set <- function(y = matrix()) {
         x <<- y
@@ -23,12 +23,12 @@ makeCacheMatrix <- function(x = matrix()) {
     ## creating a function called 'set', which takes an argument, y, and 
     ## re-assigns it to be x. Allows the user to re-assign a value to x and 
     ## clear the cached inverse variable. y also defaults to an empty matrix 
-    ## if it is not specified
+    ## if it is not specified.
     
     get <- function() {
         x
     }
-    ## the function 'get' returns x
+    ## the function 'get' returns x.
     
     solveInverse <- function(...) {
         inverse <<- solve(x, ...)
@@ -37,12 +37,12 @@ makeCacheMatrix <- function(x = matrix()) {
     ## 'solveInverse' computes the inverse of x using the 'solve' function 
     ## included in R. The inverse returned from the 'solve' function is then 
     ## assigned to the inverse variable created in line 11. This inverse is 
-    ## then returned. Additional arguments may be passed in
+    ## then returned. Additional arguments may be passed in.
     
     getInverse <- function() {
         inverse
     }
-    ## 'getInverse' returns the inverse calculated by 'solveInverse' 
+    ## 'getInverse' returns the inverse calculated by 'solveInverse.' 
     
     list(set = set,
          get =  get,
@@ -50,14 +50,14 @@ makeCacheMatrix <- function(x = matrix()) {
          getInverse = getInverse)    
 }
     ## creating a list that stores the values of 'set', 'get', 
-    ## 'solveInverse' and 'getInverse' under their respective names
+    ## 'solveInverse' and 'getInverse' under their respective names.
 
 
 ## The goal of 'cacheSolve' is to retrieve the inverse of a function 
 ## either by calling the 'solveInverse' function from line 30-33, or 
 ## by getting the cached data. It requires an argument, x, but takes 
 ## additional arguments if specified. The first time through, 'cacheSolve' 
-## will assign to a variable, inverse, the 'getInverse' value found in x. 
+## will assign to a variable, inverse, whatever 'getInverse' returns. 
 ## If it's NULL, 'cacheSolve' will call 'solveInverse' from 'makeCacheMatrix.' 
 ## 'solveInverse' uses the 'solve' function to calculate the inverse of x.
 ## This value is then returned. The second time through, 'cacheSolve' will 
@@ -70,7 +70,7 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
     inverse <- x$getInverse()
     ## creating a new variable, inverse, and assigning the value to be 
-    ## whatever the 'getInverse' found in x equals
+    ## whatever the 'getInverse' function returns.
     
     if(!is.null(inverse)) {
         message("Getting Cached Data")
